309ba9de899fecb5ef87de51d36fbcd1aa685d2b
Fredins <fredin.martin@gmail.com>
2021-12-12 16:24:42 +0100

    add css support, and style header in play screen

--- Main.hs
+++ Main.hs
@@ -5,15 +5,21 @@
 {-# LANGUAGE TupleSections #-}
 
 module Main where
+import           Control.Concurrent.Async       ( async )
 import           Control.Monad
+import           Data.ByteString                ( ByteString )
 import qualified Data.Map                      as M
 import           Data.Maybe
-import           Data.Text                      ( pack, Text )
+import           Data.Text                      ( Text
+                                                , pack
+                                                )
 import           Data.Tuple.Extra               ( both )
 import           Data.Vector                    ( Vector
                                                 , fromList
                                                 )
 import           Debug.Trace
+import           GI.Gdk.Objects                 ( screenGetDefault )
+import           GI.Gio.Interfaces.File
 import qualified GI.Gtk                        as G
 import           GI.Gtk.Declarative
 import           GI.Gtk.Declarative.App.Simple
@@ -26,11 +32,11 @@ data Screen = StartMenu
             | GameOver
 
 data State = State
-  { activeP   :: Player
-  , playerB   :: Player
-  , playerW   :: Player
-  , board     :: Board
-  , screen    :: Screen
+  { activeP :: Player
+  , playerB :: Player
+  , playerW :: Player
+  , board   :: Board
+  , screen  :: Screen
   }
 
 data Event = Close
@@ -64,66 +70,97 @@ update' s@State {..} e = case e of
 view' :: State -> AppView G.Window Event
 view' s@State {..} =
   bin G.Window [#title := "Othello", on #destroy Close] $ case screen of
-    StartMenu ->
-      widget G.Button [#label := "Player vs Player", on #clicked StartPvP]
+    StartMenu -> widget
+      G.Button
+      [ classes ["start_menu"]
+      , #label := "Player vs Player"
+      , on #clicked StartPvP
+      ]
 
     Play -> container
       G.Box
-      [#orientation := G.OrientationVertical]
-      [ BoxChild defaultBoxChildProperties $ container
-        G.Box
-        [#orientation := G.OrientationHorizontal]
-        [ BoxChild defaultBoxChildProperties { padding = 5 }
-                   (widget G.Image [#file := "gui/white1.png"])
-        , BoxChild
-          defaultBoxChildProperties { padding = 5 }
-          (widget G.Label [#label := pack (show (points playerW) ++ "x")])
-        , BoxChild
-          defaultBoxChildProperties { padding = 50 }
-          (widget G.Label [#label := pack (show (disk activeP) ++ "'s turn")])
-        , BoxChild defaultBoxChildProperties { padding = 5 }
-                   (widget G.Image [#file := "gui/black1.png"])
-        , BoxChild
-          defaultBoxChildProperties { padding = 5 }
-          (widget G.Label [#label := pack (show (points playerB) ++ "x")])
-        ]
+      [classes ["play"], #orientation := G.OrientationVertical]
+      [ BoxChild defaultBoxChildProperties $ header s
       , BoxChild defaultBoxChildProperties $ grid s
       ]
 
     GameOver -> widget G.Label [#label := pack winner]
  where
   (pb, pw) = updatePoints board
-  winner | pb > pw   = "Black wins with " ++ show pb ++ " vs " ++ show pw ++ " points"
-         | pw > pb   = "White wins with " ++ show pw ++ " vs " ++ show pb ++ " points"
-         | otherwise = "Draw!"
+  winner
+    | pb > pw = "Black wins with " ++ show pb ++ " vs " ++ show pw ++ " points"
+    | pw > pb = "White wins with " ++ show pw ++ " vs " ++ show pb ++ " points"
+    | otherwise = "Draw!"
+
+header :: State -> Widget Event
+header State {..} = container
+  G.Box
+  [classes ["header"], #orientation := G.OrientationHorizontal, #halign := G.AlignCenter]
+  [ container G.Box
+    [#orientation := G.OrientationHorizontal]
+    [ BoxChild defaultBoxChildProperties { padding = 10 } $ widget G.Image [#file := "gui/white1.png"]
+    , widget G.Label [#label := pack (show $ points playerW)]
+    ]
+  , BoxChild defaultBoxChildProperties { padding = 100 } $ widget G.Label [classes ["header_center"], #label := pack (show (disk activeP) ++ "'s turn")]
+  , container
+    G.Box
+    [#orientation := G.OrientationHorizontal]
+    [ BoxChild defaultBoxChildProperties { padding = 10 } $ widget G.Image [#file := "gui/black1.png"]
+    , widget G.Label [#label := pack (show $ points playerB)]
+    ]
+  ]
+
+
 
 
 grid :: State -> Widget Event
-grid s@State {..} = container G.Grid [] cs
+grid s@State {..} = container G.Grid [classes ["grid"]] cs
  where
   cs = fromList $ map c $ M.toList board
   c pd@((y, x), d) = GridChild
     defaultGridChildProperties { leftAttach = fromIntegral x
                                , topAttach  = fromIntegral y
                                }
-    (bin G.Button [on #clicked $ DiskClicked pd] $ widget G.Image [#file := path pd])
+    ( bin G.Button [on #clicked $ DiskClicked pd]
+    $ widget G.Image [#file := path pd]
+    )
 
   path :: (Pos, Maybe Disk) -> Text
   path (_, Just White) = "gui/white.png"
   path (_, Just Black) = "gui/black.png"
-  path (p, _) | p `elem` (fst <$> possibleMoves activeP board) = "gui/highlight.png"
-              | otherwise                                      = "gui/empty.png"
+  path (p, _)
+    | p `elem` (fst <$> possibleMoves activeP board) = "gui/highlight.png"
+    | otherwise = "gui/empty.png"
 
 
--- TODO add css support
 main :: IO ()
-main = void $ run App { view    = view'
-                      , update       = update'
-                      , inputs       = []
-                      , initialState = State { activeP   = pB
-                                             , playerB   = pB
-                                             , playerW   = pW
-                                             , board     = startingBoard
-                                             , screen    = StartMenu } }
-  where pB = Player Black 0
-        pW = Player White 0
+main = do
+  void $ G.init Nothing
+
+  s <- maybe (fail "No screen?!") return =<< screenGetDefault
+  p <- G.cssProviderNew
+  f <- fileNewForPath "gui/styles.css"
+  G.cssProviderLoadFromFile p f
+  G.styleContextAddProviderForScreen
+    s
+    p
+    (fromIntegral G.STYLE_PROVIDER_PRIORITY_USER)
+
+  void . async $ do
+    void $ runLoop app
+    G.mainQuit
+  G.main
+ where
+  app = App
+    { view         = view'
+    , update       = update'
+    , inputs       = []
+    , initialState = State { activeP = pB
+                           , playerB = pB
+                           , playerW = pW
+                           , board   = startingBoard
+                           , screen  = StartMenu
+                           }
+    }
+  pB = Player Black 0
+  pW = Player White 0
